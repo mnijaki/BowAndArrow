931dc21792593e260393809b383665eb84793775
mnijaki <mnijaki@gmail.com>
2022-06-06 23:18:14 +0200

    * Added CameraSwitcher script

---
+++ Assets/Scripts/Main/Runtime/Cameras/CameraSwitcher.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BAA.InputHandling;
+using Cinemachine;
+using UnityEngine;
+
+namespace BAA
+{
+    public class CameraSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private InputReader _inputReader;
+        [SerializeField]
+        private int _priorityBoostAmount = 10;
+
+        private CinemachineVirtualCamera _virtualCamera;
+
+        private void Awake()
+        {
+            _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        }
+
+        private void OnEnable()
+        {
+            _inputReader.aimStartedEvent += OnAimStarted;
+            _inputReader.aimFinishedEvent += OnAimFinished;
+        }
+
+        private void OnDisable()
+        {
+            _inputReader.aimStartedEvent -= OnAimStarted;
+            _inputReader.aimFinishedEvent -= OnAimFinished;
+        }
+
+        private void OnAimFinished()
+        {
+            //_virtualCamera.Priority += _priorityBoostAmount;
+        }
+
+        private void OnAimStarted()
+        {
+            //_virtualCamera.Priority -= _priorityBoostAmount;
+        }
+    }
+}
